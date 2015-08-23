bf3c27a7011c75a64a151425792030bade7e1ff3
Nikolas Bravo <nikolasbra@gmail.com>
2015-08-23 17:20:52 -0300

    example half way

--- tarea.js
+++ tarea.js
@@ -25,3 +25,23 @@ function lift(answerFun, stateFun) {
         };
     };
 };
+
+function sqr(n) {
+    return n * n;
+}
+
+function note(n){
+    console.log(n);
+}
+
+var mSqr2 = lift(sqr);
+var mNote2 = lift(note, _.identity);
+var mNeg2 = lift(function(n) { return -n });
+
+
+var negativeSqrAction2 = actions([mSqr2(), mNote2(), mNeg2()], function(_, state) {
+    return state;
+});
+
+negativeSqrAction2(100); // NOTE: 10000
+//=> -10000
