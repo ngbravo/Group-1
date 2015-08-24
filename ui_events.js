804a367df3a88fa92b2c65c8c0dfbaaf79c51480
Nikolas Bravo <nikolasbra@gmail.com>
2015-08-23 23:15:34 -0300

    jquery complete

--- ui_events.js
+++ ui_events.js
@@ -4,17 +4,26 @@ $( document ).ready(function() {
         var filterType = $(this).attr('id').slice(9);
         if($(this).is(":checked")) {
             var filterQuantity = $('#quantity-'+filterType).val();
+            if(filterQuantity === ""){
+                filterQuantity = 0;
+            }
             var toAppend = "<li id='"+filterType+"-li'><span class='filterType'>"+filterType+"</span>, q:<span class='filterQuantity'>"+filterQuantity+"</span></li>";
             $('#ordered-list').append(toAppend);
         }
         else{
             $("#"+filterType+"-li").remove()
         }
+    });
 
+    $('.numberBox').change(function() {
+        var newText = $(this).val();
+        var filterType = $(this).attr('id').slice(9);
+        if($("#checkbox-"+filterType).is(":checked")) {
+            $("#"+filterType+"-li").find(".filterQuantity").text(newText);
+        }
     });
 
     $("#button").click(function(){
-
         filtersArray = [];
         $("li").each(function() {
             var type = $(this).find(".filterType").text();
