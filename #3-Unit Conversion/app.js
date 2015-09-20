347b42116906687feb59fd0c59baf1ba9c9bc5ff
Nikolas Bravo <nikolasbra@gmail.com>
2015-09-20 19:28:57 -0300

    local bootstrap and angular

--- #3-Unit Conversion/app.js
+++ #3-Unit Conversion/app.js
@@ -1,1 +1,39 @@
 var app = angular.module('conversor', [ ]);
+
+app.controller('ConversionController', function(){})
+
+var unitCategories = [
+  {
+    name: "length",
+    baseUnit: "meter",
+    units:[
+      {
+        name: "meter",
+        conversionSlope: 1,
+        conversionOffset: 0
+      },
+      {
+        name: "kilometer",
+        conversionSlope: 0.001, //meter = kilometer * conversionSlope + conversionOffset
+        conversionOffset: 0
+      }
+    ]
+  },
+  {
+    name: "weight",
+    baseUnit: "gram",
+    units:[
+      {
+        name: "gram",
+        conversionSlope: 1,
+        conversionOffset: 0
+      },
+      {
+        name: "kilogram",
+        conversionSlope: 0.001,
+        conversionOffset: 0
+      }
+    ]
+  }
+
+];
