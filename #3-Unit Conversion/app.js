d3d8ab3b3f6e5f4f8ff44636d01f90fa22fb3775
Nikolas Bravo <nikolasbra@gmail.com>
2015-09-20 21:51:54 -0300

    conversion for one unit

--- #3-Unit Conversion/app.js
+++ #3-Unit Conversion/app.js
@@ -1,19 +1,36 @@
 var app = angular.module('conversor', [ ]);
 
-app.controller('ConversionController', function(){})
+app.controller('ConversionController', function(){
+  this.categories = unitCategories;
+  this.selectedCategory = this.categories[0];
+  this.currentUnit = null;
+  this.convertedUnit = null;
+  this.currentUnitValue = 0;
+  this.convertedUnitValue = "N/A";
+  this.updateConvertedUnitValue = function(){
+    try {
+      var toBase = this.currentUnitValue * this.convertedUnit.conversionSlope + this.convertedUnit.conversionOffset;
+      this.convertedUnitValue = (toBase-this.currentUnit.conversionOffset)/this.currentUnit.conversionSlope;
+    }
+    catch(err) {
+        this.convertedUnitValue = "N/A";
+    }
+  };
+});
 
 var unitCategories = [
   {
     name: "length",
-    baseUnit: "meter",
     units:[
       {
         name: "meter",
+        isBaseUnit: true,
         conversionSlope: 1,
         conversionOffset: 0
       },
       {
         name: "kilometer",
+        isBaseUnit: false,
         conversionSlope: 0.001, //meter = kilometer * conversionSlope + conversionOffset
         conversionOffset: 0
       }
@@ -21,15 +38,16 @@ var unitCategories = [
   },
   {
     name: "weight",
-    baseUnit: "gram",
     units:[
       {
         name: "gram",
+        isBaseUnit: true,
         conversionSlope: 1,
         conversionOffset: 0
       },
       {
         name: "kilogram",
+        isBaseUnit: false,
         conversionSlope: 0.001,
         conversionOffset: 0
       }
