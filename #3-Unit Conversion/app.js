52a8587dc6891e1eb558a9795d2afc7b7d1c379a
Nikolas Bravo <nikolasbra@gmail.com>
2015-09-20 23:18:09 -0300

    added units

--- #3-Unit Conversion/app.js
+++ #3-Unit Conversion/app.js
@@ -27,6 +27,8 @@ app.controller('ConversionController', function(){
   };
 });
 
+//base * conversionSlope + conversionOffset = newUnit
+//conversiones son de cualquier unidad a la base
 var unitCategories = [
   {
     name: "length",
@@ -40,7 +42,7 @@ var unitCategories = [
       {
         name: "kilometer",
         isBaseUnit: false,
-        conversionSlope: 1000, //base * conversionSlope + conversionOffset = newUnit
+        conversionSlope: 1000,
         conversionOffset: 0
       },
       {
@@ -74,14 +76,141 @@ var unitCategories = [
     units:[
       {
         name: "gram",
+        isBaseUnit: false,
+        conversionSlope: 0.001,
+        conversionOffset: 0
+      },
+      {
+        name: "kilogram",
         isBaseUnit: true,
         conversionSlope: 1,
         conversionOffset: 0
       },
       {
-        name: "kilogram",
+        name: "pound",
+        isBaseUnit: false,
+        conversionSlope: 0.45359237,
+        conversionOffset: 0
+      },
+      {
+        name: "metric ton",
+        isBaseUnit: false,
+        conversionSlope: 1000,
+        conversionOffset: 0
+      },
+    ]
+  },
+  {
+    name: "area",
+    units:[
+      {
+        name: "square meter",
+        isBaseUnit: true,
+        conversionSlope: 1,
+        conversionOffset: 0
+      },
+      {
+        name: "hectare",
+        isBaseUnit: false,
+        conversionSlope: 10000,
+        conversionOffset: 0
+      }
+    ]
+  },
+  {
+    name: "volume",
+    units:[
+      {
+        name: "cubic meter",
+        isBaseUnit: true,
+        conversionSlope: 1,
+        conversionOffset: 0
+      },
+      {
+        name: "litre",
         isBaseUnit: false,
         conversionSlope: 0.001,
+        conversionOffset: 0
+      }
+    ]
+  },
+  {
+    name: "temperature",
+    units: [
+      {
+        name: "celcius",
+        isBaseUnit: true,
+        conversionSlope: 1,
+        conversionOffset: 0
+      },
+      {
+        name: "farenheit",
+        isBaseUnit: false,
+        conversionSlope: 5/9,
+        conversionOffset: -32*5/9
+      },
+      {
+        name: "kelvin",
+        isBaseUnit: false,
+        conversionSlope: 1,
+        conversionOffset: -273.15
+      }
+    ]
+  },
+  {
+    name: "speed",
+    units: [
+      {
+        name: "m/s",
+        isBaseUnit: true,
+        conversionSlope: 1,
+        conversionOffset: 0
+      },
+      {
+        name: "km/h",
+        isBaseUnit: false,
+        conversionSlope: 0.277777778,
+        conversionOffset: 0
+      },
+      {
+        name: "mile/h",
+        isBaseUnit: false,
+        conversionSlope: 0.44704,
+        conversionOffset: 0
+      }
+    ]
+  },
+  {
+    name: "pressure",
+    units: [
+      {
+        name: "Pascal",
+        isBaseUnit: true,
+        conversionSlope: 1,
+        conversionOffset: 0
+      },
+      {
+        name: "atm",
+        isBaseUnit: false,
+        conversionSlope: 101325,
+        conversionOffset: 0
+      },
+      {
+        name: "bar",
+        isBaseUnit: false,
+        conversionSlope: 100000,
+        conversionOffset: 0
+      },
+      {
+        name: "Torr",
+        isBaseUnit: false,
+        conversionSlope: 133.322368,
+        conversionOffset: 0
+      },
+      {
+        name: "psi",
+        isBaseUnit: false,
+        conversionSlope: 6894.75729,
         conversionOffset: 0
       }
     ]
